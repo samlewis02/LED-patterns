2bf703a672ae31b15f5ff0081b74031712b7ab29
samlewis02 <samlewis02@users.noreply.github.com>
2018-05-25 19:36:59 +0800

    Add files via upload

---
+++ MySSID.h
@@ -0,0 +1,16 @@
+#ifndef MySSID_h
+#define MySSID_h
+
+// the #include statment and code go here...
+// Replace with your network credentials
+static const char* ssid = "P874-21-2-3F";
+static const char* password = "077190028";
+static const char* WIFI_SSID =      "P874-21-2-3F";
+static const char* WIFI_PASS  =     "077190028";
+// Adafruit IO username and key
+static const char* IO_USERNAME  =  "samlewis02";
+static const char* IO_KEY   =      "f0cc7228455645bb8aec73b603557591";
+//UK
+//static const char* ssid = "VodafoneMobileWiFi-393A26";
+//static const char*  pass = "2359547235";
+#endif
