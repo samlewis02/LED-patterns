660837c690ae500408ebdb77fb81a88f8a9f81b2
samlewis02 <samlewis02@users.noreply.github.com>
2018-05-25 19:39:26 +0800

    Update MySSID.h

--- MySSID.h
+++ MySSID.h
@@ -3,14 +3,11 @@
 
 // the #include statment and code go here...
 // Replace with your network credentials
-static const char* ssid = "P874-21-2-3F";
-static const char* password = "077190028";
-static const char* WIFI_SSID =      "P874-21-2-3F";
-static const char* WIFI_PASS  =     "077190028";
+static const char* ssid = "your_ssid";
+static const char* password = "your_password";
+static const char* WIFI_SSID =      "your_ssid";
+static const char* WIFI_PASS  =     "your_password";
 // Adafruit IO username and key
-static const char* IO_USERNAME  =  "samlewis02";
-static const char* IO_KEY   =      "f0cc7228455645bb8aec73b603557591";
-//UK
-//static const char* ssid = "VodafoneMobileWiFi-393A26";
-//static const char*  pass = "2359547235";
+static const char* IO_USERNAME  =  "your-AdafruitIO-username";
+static const char* IO_KEY   =      "your-AdafruitIO-key";
 #endif
